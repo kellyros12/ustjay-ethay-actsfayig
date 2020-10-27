047c9ca3bd86da220fa26e29937edb559d152770
Kelly Ros <kellyros91@gmail.com>
2020-10-26 22:51:14 -0700

    Initial commit.

--- main.py
+++ main.py
@@ -19,10 +19,16 @@ def get_fact():
 
 @app.route('/')
 def home():
-    return "FILL ME!"
+    fact = get_fact().strip()
+    payload = {'input_text': fact}
+    response = requests.get('http://hidden-journey-62459.herokuapp.com/',
+                            allow_redirects=False)
+
+    body = requests.post('http://hidden-journey-62459.herokuapp.com/piglatinize/',
+                         data=payload)
+    return body.url
 
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6787))
     app.run(host='0.0.0.0', port=port)
-
